dbdc07d75a256377ad02121ce941c65f6d8e32f0
Clive Hoggart <clivehoggart@gmail.com>
2025-11-04 14:49:15 -0500

    ...

--- function_h2.R
+++ function_h2.R
@@ -150,16 +150,17 @@ h2.rare.big2 <- function( prop.in.tail, tail=0.01, rare.maf=1e-4, beta, mu.y=0, 
         var.rare[i] <- m[i] * 2*rare.maf*(1-rare.maf) * beta^2
     }
 
-    h2 <- sum(var.rare) / (1+sum(var.rare))
+    h2 <- sum(var.rare)
     ret <- c( h2, sum.rare.freq, m )
     names(ret) <- c( 'h2', 'sum.rare.freq1', 'sum.rare.freq2',
                     'm1', 'm2' )
     return(as.list(ret))
 }
 
-h2.est.emp <- function( n, effect.size, prs.r2, h2.common, beta, sd.beta=0, rare.maf=1e-4, tail=0.01 ){
-    ret <- matrix(ncol=5,nrow=0)
-    colnames(ret) <- c( 'h2', 'sum.rare.freq1', 'sum.rare.freq2', 'm1', 'm2' )
+h2.est.emp <- function( n, effect.size, prs.r2, h2.common, beta, sd.beta=0, rare.maf=1e-4, tail=0.01, iter.max=5 ){
+    good.sim <- FALSE
+    ret <- matrix(ncol=6,nrow=0)
+    colnames(ret) <- c( 'h2', 'sum.rare.freq1', 'sum.rare.freq2', 'm1', 'm2', 'iter' )
     p.in.tail <- est.prop.in.tail2( effect.size, beta=beta, r2=prs.r2 )
     p.in.tail <- ifelse( p.in.tail<0, 0, p.in.tail )
     p.in.tail <- ifelse( p.in.tail>1, 1, p.in.tail )
@@ -170,101 +171,150 @@ h2.est.emp <- function( n, effect.size, prs.r2, h2.common, beta, sd.beta=0, rare
         m1 <- m1/2
         m2 <- m2/2
     }
-#    print( 2 * rare.maf * (1-rare.maf) * (m1+m2) * beta^2 )
-    for( i in 1:15 ){
+    m1.i <- m1
+    m2.i <- m2
+    for( i in 1:20 ){
+        print(i)
         m1.new <- 1e6
         m2.new <- 1e6
-        while( h2.common + 2 * rare.maf * (1-rare.maf) * (m1.new+m2.new) * beta^2 > 0.95 ){
-#            print(c(m1,m2))
+        iter.sum <- 0
+        while( (h2.common + 2 * rare.maf * (1-rare.maf) * (m1.new+m2.new) * beta^2 > 0.95) & iter.sum < iter.max )
+        {
+            iter.sum <- iter.sum + 1
             sim.data <- sim.pheno( n=n, m1=m1, m2=m2, rare.maf=rare.maf, beta=beta,
-                                  h2.common=h2.common, prs.r2=prs.r2, sd.beta=sd.beta )
+                                  h2.common=h2.common, prs.r2=prs.r2, effect.size, sd.beta=sd.beta, tail=tail )
+            print(sim.data[[3]])
             mu.y = mean(sim.data[[2]][,'y.prime'])
             sigma.y = sd(sim.data[[2]][,'y.prime'])
-            p.in.tail <- est.prop.in.tail.emp( effect.size, beta,
+            p.in.tail <- est.prop.in.tail.emp( effect.size, beta, r2=prs.r2,
                                               y.prime=sim.data[[2]]$y.prime, prs=sim.data[[2]]$prs.prime )
-            mu.y <- mean( sim.data[[2]][,'y.prime'] )
-            sigma.y <- sd( sim.data[[2]][,'y.prime'] )
             p.in.tail2 <- est.prop.in.tail2( effect.size, beta, r2=prs.r2, mu.y=mu.y, sigma.y=sigma.y )
             p.in.tail <- ifelse( ( is.nan(p.in.tail) | p.in.tail<0 | p.in.tail>1 ) , p.in.tail2, p.in.tail )
             p.in.tail <- ifelse( p.in.tail<0, 0, p.in.tail )
             p.in.tail <- ifelse( p.in.tail>1, 1, p.in.tail )
-#        print(unlist(p.in.tail))
             ex <- h2.rare.big.emp( p.in.tail, beta=beta,
                                   y.prime=sim.data[[2]]$y.prime, prs=sim.data[[2]]$prs.prime )
-#        print(unlist(ex))
             m1.new <- ifelse( ex$m1<0, 0, round(ex$m1) )
             m2.new <- ifelse( ex$m2<0, 0, round(ex$m2) )
             m1.new <- ifelse( is.na(ex$m1), 1e6, m1.new )
             m2.new <- ifelse( is.na(ex$m2), 1e6, m2.new )
-#            print(c(m1.new,m2.new))
-            print( h2.common + 2 * rare.maf * (1-rare.maf) * (m1.new+m2.new) * beta^2 )
+            print(unlist(ex))
+#            print( h2.common + 2 * rare.maf * (1-rare.maf) * (m1.new+m2.new) * beta.assumed^2 )
         }
-        m1 <- m1.new
-        m2 <- m2.new
-        if( i>5 ){
-            ret <- rbind( ret, unlist(ex) )
+        if( iter.sum==iter.max ){
+            m1 <- m1.i
+            m2 <- m2.i
+        }else{
+            m1 <- m1.new
+            m2 <- m2.new
+            m1.i <- m1
+            m2.i <- m2
+        }
+        if( (h2.common + 2 * rare.maf * (1-rare.maf) * (m1.new+m2.new) * beta^2 < 0.95) ){
+            ret <- rbind( ret, c( unlist(ex), iter.sum ) )
         }
     }
-    return( apply( ret, 2, mean ) )
+    if( nrow(ret)>1 ){
+        ret1 <- c( apply( ret, 2, mean ), nrow(ret) )
+    }else{
+        ret1 <- c( ret, nrow(ret) )
+    }
+    return( ret )
 }
 
-est.prop.in.tail.emp <- function( effect.size, beta, tail=0.01, y.prime, prs ){
+est.prop.in.tail.emp <- function( effect.size, beta, r2, y.prime, prs, tail=0.01 ){
+    r <- sqrt(r2)
     kappa <- qnorm( tail, lower.tail=FALSE )
     prop.in.tail <- c(0,0)
 
     m.Y99 <- mean( prs[ y.prime < -kappa ] )
     m.Y99.rare <- mean( prs[ y.prime < (-kappa+beta) ] )
-    prop.in.tail[1] <- -effect.size[1] / ( m.Y99 - m.Y99.rare ) # negative since forced +ve effect for tail
+    m.Y99.null <- -r * dnorm( kappa ) / pnorm( kappa, lower.tail=FALSE )
+    prop.in.tail[1] <- (-effect.size[1] - m.Y99.null + m.Y99 ) / ( m.Y99 - m.Y99.rare ) # negative since forced +ve effect for tail
 
     m.Y99 <- mean( prs[ y.prime > kappa ] )
     m.Y99.rare <- mean( prs[ y.prime > (kappa-beta) ] )
-    prop.in.tail[2] <- effect.size[2] / ( m.Y99 - m.Y99.rare )
+    m.Y99.null <- r * dnorm( kappa ) / pnorm( kappa, lower.tail=FALSE )
+    prop.in.tail[2] <- ( effect.size[2] - m.Y99.null + m.Y99 ) / ( m.Y99 - m.Y99.rare )
 
     return( prop.in.tail )
 }
 
 h2.rare.big.emp <- function( prop.in.tail,  tail=0.01, rare.maf=1e-4, beta, y.prime, prs ){
 # beta -- presumed effect size of rare large effect variants
-    kappa <- qnorm( tail, lower.tail=FALSE )
+    mu.y = mean(y.prime)
+    sigma.y = sd(y.prime)
+    K <- qnorm( tail, lower.tail=FALSE )
+    K.prime = vector()
+    K.prime.rare = vector()
+    K.prime[1] = (-K - mu.y)/sigma.y
+    K.prime[2] = (K - mu.y)/sigma.y
+    K.prime.rare[1] = (-K + beta - mu.y)/sigma.y
+    K.prime.rare[2] = (K - beta - mu.y)/sigma.y
 
     sum.rare.freq <- vector()
     var.rare <- vector()
     m <- c(0,0)
 
-    sum.rare.freq[1] <- prop.in.tail[1]*mean( y.prime < -kappa ) /
-        ( prop.in.tail[1]*mean( y.prime < -kappa ) + (1-prop.in.tail[1])*mean( y.prime<(-kappa+beta) ) )
-    sum.rare.freq[2] <- prop.in.tail[2]*mean( y.prime > kappa ) /
-        ( prop.in.tail[2]*mean( y.prime > kappa ) + (1-prop.in.tail[2])*mean( y.prime>(kappa-beta) ) )
+    sum.rare.freq[1] <- prop.in.tail[1]*mean( y.prime < -K ) /
+        ( prop.in.tail[1]*mean( y.prime < -K ) + (1-prop.in.tail[1])*mean( y.prime<(-K+beta) ) )
+    sum.rare.freq[2] <- prop.in.tail[2]*mean( y.prime > K ) /
+        ( prop.in.tail[2]*mean( y.prime > K ) + (1-prop.in.tail[2])*mean( y.prime>(K-beta) ) )
+
+#    print( c( prop.in.tail[2], mean( y.prime > K ), sum( y.prime > K )  ) )
+#    print(sum.rare.freq)
+
+    if( sum( y.prime < -K ) < 2 ){
+        print('here1')
+        print( sum( y.prime < -K ) )
+        sum.rare.freq[1] <- prop.in.tail[1]*pnorm( K.prime[1], lower.tail=TRUE ) /
+            ( prop.in.tail[1]*pnorm( K.prime[1], lower.tail=TRUE ) +
+              (1-prop.in.tail[1])*pnorm( K.prime.rare[1], lower.tail=TRUE ) )
+        }
+    if( sum( y.prime > K ) < 2 ){
+        print('here2')
+        print(sum( y.prime > K ))
+        sum.rare.freq[2] <- prop.in.tail[2]*pnorm( K.prime[2], lower.tail=FALSE ) /
+            ( prop.in.tail[2]*pnorm( K.prime[2], lower.tail=FALSE ) +
+              (1-prop.in.tail[2])*pnorm( K.prime.rare[2], lower.tail=FALSE ) )
+    }
+#    print(sum.rare.freq)
 
     for( i in 1:2 ){
         m[i] <- 0.5*sum.rare.freq[i] / rare.maf # number of rare large effect variants
         var.rare[i] <- m[i] * 2*rare.maf*(1-rare.maf) * beta^2
     }
 
-    h2 <- sum(var.rare) / (1+sum(var.rare))
+    h2 <- sum(var.rare)
     ret <- c( h2, sum.rare.freq, m )
     names(ret) <- c( 'h2', 'sum.rare.freq1', 'sum.rare.freq2',
                     'm1', 'm2' )
     return(as.list(ret))
 }
 
-sim.pheno <- function( n, m1, m2, rare.maf, beta, h2.common, prs.r2, sd.beta=0, obs.prs.test ){
+sim.pheno <- function( n, m1, m2, rare.maf, beta, h2.common, prs.r2, effect.size, sd.beta=0, tail=0.01 ){
 
-#    while( test['lower','ci.lower'] < obs.prs.test['lower'] |
-#           test['lower','ci.upper'] > obs.prs.test['lower'] |
-#           test['upper','ci.lower'] < obs.prs.test['upper'] |
-#           test['upper','ci.upper'] > obs.prs.test['upper'] ){
-#               test <- prs.test()
-#    }
+    #    while( s.upper<5 | s.lower<5 ){
+#    K <- qnorm( tail, lower.tail=FALSE )
+#    s.lower <- 0
+#    s.upper <- 0
+#        s.lower <- sum( y.prime < -K )
+#        s.upper <- sum( y.prime > K )
+#        print( c( s.upper, s.lower ) )
 
+    test <- matrix(nrow=2,ncol=5,data=0)
+    colnames(test) <- c( 'effect', 'ci.lower', 'ci.upper', 'p', 'se' )
+    rownames(test) <- c( 'lower', 'upper' )
     common.effects <- sqrt(h2.common) * rnorm( n=n )
     if( m1>0 ){
-        rare.effects1 <- apply( matrix(ncol=n,data=replicate( n, rnorm( m1, -beta, sd.beta ) * rbinom(n=m1,p=rare.maf,size=2))), 2, sum )
+        rare.effects1 <- apply( matrix(ncol=n,data=replicate( n, rnorm( m1, -beta, sd.beta ) *
+                                                                 rbinom(n=m1,p=rare.maf,size=2))), 2, sum )
     }else{
         rare.effects1 <- rep(0,n)
     }
     if( m2>0 ){
-        rare.effects2 <- apply( matrix(ncol=n,data=replicate( n, rnorm( m2, beta, sd.beta ) * rbinom(n=m2,p=rare.maf,size=2))), 2, sum )
+        rare.effects2 <- apply( matrix(ncol=n,data=replicate( n, rnorm( m2, beta, sd.beta ) *
+                                                                 rbinom(n=m2,p=rare.maf,size=2))), 2, sum )
     }else{
         rare.effects2 <- rep(0,n)
     }
@@ -272,23 +322,28 @@ sim.pheno <- function( n, m1, m2, rare.maf, beta, h2.common, prs.r2, sd.beta=0, 
     env <- sqrt(h2.env) * rnorm( n=n )
     y <- common.effects + rare.effects1 + rare.effects2 + env
     yy <- quantile.normalise(y)
-
-
     prs.e <- h2.common * ( h2.common / prs.r2 - 1 )
     prs <- common.effects + sqrt(prs.e) * rnorm( n=n )
     delta.prs.r2 <- cor( prs, yy )^2 - prs.r2
     prs.ee <- prs.e / 10
-    while( delta.prs.r2>0.0005 ){
+    while( delta.prs.r2>0.001 ){
         prs <- prs + sqrt(prs.ee) * rnorm( n=n )
         delta.prs.r2 <- cor( prs, yy )^2 - prs.r2
     }
     prs <- prs / sd(prs)
+    test <- prs.test( prs, yy )
+#    print(test)
+    good.sim <-
+        ifelse( test['lower','ci.lower'] < effect.size[1]&effect.size[1] < test['lower','ci.upper'] &
+                test['upper','ci.lower'] < effect.size[2]&effect.size[2] < test['upper','ci.upper'],
+               TRUE, FALSE )
 
     ptr <- which( rare.effects1==0 & rare.effects2==0 )
     y.prime <- yy[ptr]
     prs.prime <- prs[ptr]
 
-    return( list( data.frame( y, yy, prs, rare.effects1, rare.effects2 ) , data.frame( y.prime, prs.prime ) ) )
+    return( list( data.frame( y, yy, prs, rare.effects1, rare.effects2 ),
+                 data.frame( y.prime, prs.prime ),good.sim ) )
 }
 
 sim <- function( n, m, rare.maf, beta, prs.r2, centre.reg=NULL ){
